c3e718b6de7e641f223f8fb60b57b8c3e4b1ef14
Edwin Codrington <edwincodrington@gmail.com>
2023-02-12 15:19:50 -0500

    refactored switches queries for new data columns (whos genius idea was this?)

--- back-end/queries/switches.js
+++ back-end/queries/switches.js
@@ -17,10 +17,36 @@ const getSwitch = async (id) => {
 
 const createSwitch = async (keyswitch) => {
 	try {
-		let { name, brand, type, force, prelubed, image } = keyswitch;
+		let {
+			name,
+			brand,
+			type,
+			top_housing,
+			bottom_housing,
+			stem,
+			travel_distance,
+			force,
+			three_pin,
+			prelubed,
+			image,
+			price_per_switch,
+		} = keyswitch;
 		return await db.one(
-			"INSERT INTO switches (name, brand, type, force, prelubed, image) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
-			[name, brand, type, force, prelubed, image]
+			"INSERT INTO switches (name, brand, type, top_housing, bottom_housing, stem, travel_distance, force, three_pin, prelubed, image, price_per_switch) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12) RETURNING *",
+			[
+				name,
+				brand,
+				type,
+				top_housing,
+				bottom_housing,
+				stem,
+				travel_distance,
+				force,
+				three_pin,
+				prelubed,
+				image,
+				price_per_switch,
+			]
 		);
 	} catch (error) {
 		return error;
@@ -28,11 +54,38 @@ const createSwitch = async (keyswitch) => {
 };
 
 const updateSwitch = async (id, keyswitch) => {
-	let { name, brand, type, force, prelubed, image } = keyswitch;
+	let {
+		name,
+		brand,
+		type,
+		top_housing,
+		bottom_housing,
+		stem,
+		travel_distance,
+		force,
+		three_pin,
+		prelubed,
+		image,
+		price_per_switch,
+	} = keyswitch;
 	try {
 		return await db.one(
-			"UPDATE switches SET name=$1, brand=$2, type=$3, force=$4, prelubed=$5, image=$6 WHERE id=$7 RETURNING *",
-			[name, brand, type, force, prelubed, image, id]
+			"UPDATE switches SET name=$1, brand=$2, type=$3, top_housing=$4, bottom_housing=$5, stem=$6, travel_distance=$7, force=$8, three_pin=$9, prelubed=$10, image=$11, price_per_switch=$12 WHERE id=$13 RETURNING *",
+			[
+				name,
+				brand,
+				type,
+				top_housing,
+				bottom_housing,
+				stem,
+				travel_distance,
+				force,
+				three_pin,
+				prelubed,
+				image,
+				price_per_switch,
+				id,
+			]
 		);
 	} catch (error) {
 		return error;
