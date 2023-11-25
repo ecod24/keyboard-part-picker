d65459d8287a9f0d35639a9e14be98fadbcab6ff
Edwin Codrington <edwincodrington@gmail.com>
2023-11-24 21:27:18 -0500

    tailwind and a home component

--- new-ui/src/App.tsx
+++ new-ui/src/App.tsx
@@ -1,25 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from "react-router";
+import Home from "./Pages/Home/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+		<div className="App">
+			{/* <img src={logo} className="App-logo" alt="logo" />
+			<p>
+				Edit <code>src/App.tsx</code> and save to reload.
+			</p>
+			<a
+				className="App-link"
+				href="https://reactjs.org"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				Learn React
+			</a> */}
+			<Routes>
+				<Route path="/" element={<Home />} />
+			</Routes>
+		</div>
   );
 }
 
---
+++ new-ui/src/Pages/Home/Home.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+	return <div className="text-3xl font-bold underline">Home</div>;
+};
+
+export default Home;
